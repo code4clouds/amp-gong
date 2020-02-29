c78204f0870e92beca0a3d55fdc2dfd520207905
code4clouds <julio.colon@microsoft.com>
2020-02-28 18:29:30 -0800

    gong code added

--- gong.py
+++ gong.py
@@ -2,6 +2,8 @@
 
 import time, sys, os
 from azure.servicebus import QueueClient, Message
+import RPi.GPIO as GPIO
+
 
 # Create the QueueClient
 
@@ -18,12 +20,37 @@ else:
     print('QUEUE_NAME:', QUEUE_NAME)
     print('QUEUE_CONNECTION_STRING:', QUEUE_CONNECTION_STRING)
 
-    # Create teh service bus connection
-    queue_client = QueueClient.from_connection_string(QUEUE_CONNECTION_STRING, QUEUE_NAME)
+# Setup GPIO Channels
+Relay_Ch1 = 26
+Relay_Ch2 = 20
+Relay_Ch3 = 21
 
-    # Receive the message from the queue
-    while(True):
-        with queue_client.get_receiver() as messages:
-            for message in messages:
-                print(message)
-                message.complete()
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+
+GPIO.setup(Relay_Ch1,GPIO.OUT)
+GPIO.setup(Relay_Ch2,GPIO.OUT)
+GPIO.setup(Relay_Ch3,GPIO.OUT)
+
+# Create teh service bus connection
+queue_client = QueueClient.from_connection_string(QUEUE_CONNECTION_STRING, QUEUE_NAME)
+
+# Receive the message from the queue
+while(True):
+    with queue_client.get_receiver() as messages:
+        for message in messages:
+            print(message)
+            try:
+                #Control the Channel 1
+                GPIO.output(Relay_Ch1,GPIO.LOW)
+                print("Channel 1:The Common Contact is access to the Normal Open Contact!")
+                time.sleep(0.5)
+
+                GPIO.output(Relay_Ch1,GPIO.HIGH)
+                print("Channel 1:The Common Contact is access to the Normal Closed Contact!\n")
+                time.sleep(0.5)
+                # Remove message from queue
+                message.complete()  
+            except:
+                print("except")
+                GPIO.cleanup()
